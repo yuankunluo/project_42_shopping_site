94ab8c5af8371923e435f48204cae57ef107c4e5
yuankunluo <yuankun1988@gmail.com>
2014-07-14 02:32:25 +0200

    Adding static fiels like css and bootstrap

--- store/views.py
+++ store/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_GET
 from django.views.decorators.http import require_POST
+from django.http import HttpResponse
 
 
 # Create your views here.
 
 
-def homepage():
-    html = """
-        <img scr="{% static "image/404.jpg" %}">
-    """
-    render(html)
+
+def index(request):
+
+    return render(request,'store/index.html')
